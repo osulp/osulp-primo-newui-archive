7e6cb2c7233cb67d716bf84b81043273d6f33bed
Dan M <dan.moore@oregonstate.edu>
2017-08-24 16:08:17 -0700

    wip

--- OSU/js/custom.js
+++ OSU/js/custom.js
@@ -7,7 +7,7 @@ var app = angular.module('viewCustom', ['angularLoad']);
 // add chat widget to header    
 app.component('prmSearchBookmarkFilterAfter', {
     bindings: {},
-    template: '<div class="chat"><a ng-href="http://answers.library.oregonstate.edu/widget_standalone.php?hash=848ad121b384a3768c03838752654abb" target="_blank">Chat</a></div>'
+    template: '<div class="chat"><a ng-href="http://answers.library.oregonstate.edu/widget_standalone.php?hash=848ad121b384a3768c03838752654abb" target="_blank">Live Chat</a></div>'
 }); 
 
 // show/hide borrowing institutions    
@@ -127,6 +127,10 @@ app.component('prmAuthenticationAfter', {
     }
 });  
   
+})();
+
+/* In Progress 
+    
 // report a problem - not working yet   
 angular
   .module('reportProblem', [])
@@ -156,8 +160,52 @@ app.constant('reportProblemOptions', {
   button: "Report a Problem",
   base: ""
 })
+
+// OSU branding - header    
+app.component('prmshowPrimoExploreAfter', {
+    bindings: {},
+    templateUrl: 'custom/OSU/html/osu-header.html'
+});   
+        
     
-})();
+function add_custom_header(header_container)
+{
+	console.log("... in add_custom_header function");
+	var header_container = angular.element(document.getElementsByClassName('custom-header'));
+	if(header_container.length == 0)
+	{
+		var custom_header_html = '<div id="header"><div id="osu-top-hat" class="new container-fluid"><a href="http://www.oregonstate.edu"><img src="http://oregonstate.edu/themes/osu/drupal8-osuhomepage/logo.svg" alt="Oregon State University "></a> </div><h1><a href="http://library.oregonstate.edu">Libraries</a></h1><div id="mobile-icon-menu"><a href="#" id="toggle-mobile-menu" class="m-icon-link"><i class="icon-reorder"></i></a></div><div id="page-wrapper" class="container-fluid"><div id="main-menu" role="navigation"><div class="region region-nav"><div id="block-nice-menus-1" class="block block-nice-menus"><div class="menu-container" id="desktop-menu"><ul class="nice-menu nice-menu-down nice-menu-main-menu" id="nice-menu-1"><li class="menuparent"><span title="" class="menu-top-level">Borrow &amp; Request</span><ul class="menu-dropdown fade"><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/reserves" title="">Course Reserves</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/ill" title="">Interlibrary Loan</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/purchase-request" title="">Purchase Request</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/borrowing-policies" title="">Borrowing Policies</a></li></ul></li><li class="menuparent"><span title="" class="menu-top-level">Help</span><ul class="menu-dropdown fade"><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/reference" title="">Ask a Librarian</a></li><li class="menu-dropdown-item"><a href="//guides.library.oregonstate.edu/" title="">Research Guides</a></li><li class="menu-dropdown-item"><a href="//diy.library.oregonstate.edu/" title="">Library DIY</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/graduate-students" title="">Workshops</a></li></ul></li><li class="menuparent"><span title="" class="menu-top-level">Meet &amp; Study Here</span><ul class="menu-dropdown fade"><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/reserve-room" title="">Reserve a Room</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/floormaps" title="">Floor Maps</a></li></ul></li><li class="menuparent"><span title="" class="menu-top-level">Tech &amp; Print</span><ul class="menu-dropdown fade"><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/loanable-equipment" title="">Loanable Equipment</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/copy-print-scan" title="">Copy, Print, Scan</a></li><li class="menu-dropdown-item"><a href="http://guides.library.oregonstate.edu/3Dprinting" title="">3D Printers</a></li><li class="menu-dropdown-item"><a href="//is.oregonstate.edu/service/large-format-poster-production" title="">Poster Printing</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/computers" title="">Computers</a></li></ul></li><li class="menuparent"><span title="" class="menu-top-level">About</span><ul class="menu-dropdown fade"><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/about/valley" title="">About the Valley Library</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/staff/directory" title="">Directory</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/policies" title="">Policies</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/visit" title="">Visiting</a></li><li class="menu-dropdown-item"><a href="//osulibrary.oregonstate.edu/jobs" title="">Jobs</a></li></ul></li></ul></div></div></div></div></div></div>'
+		var prm_explore_main = angular.element(document.querySelector('prm-explore-main'));
+		if(prm_explore_main.length == 1)
+		{
+			prm_explore_main.after(custom_header_html);
+			var header_container = angular.element(document.getElementsByClassName('custom-header'));
+			angular.element(header_container).after(prm_explore_main);
+		}
+		
+		var prm_full_view_page = angular.element(document.querySelector('prm-full-view-page'));
+		if(prm_full_view_page.length == 1)
+		{
+			prm_full_view_page.after(custom_header_html);
+			var header_container = angular.element(document.getElementsByClassName('custom-header'));
+			header_container.after(prm_full_view_page);
+		}
+		
+		var prm_services_page = angular.element(document.querySelector('prm-services-page'));
+		if(prm_services_page.length == 1)
+		{
+			prm_services_page.after(custom_header_html);
+			var header_container = angular.element(document.getElementsByClassName('custom-header'));
+			header_container.after(prm_services_page);
+		}
+	}
+	else
+	{
+		console.log("header already exists - this function shouldn't have been called?!");
+		console.log("path name: " + window.location.pathname);
+	}
+}
+    
 
 // change name of Export RIs action
 /*document.addEventListener('DOMContentLoaded', function() {
